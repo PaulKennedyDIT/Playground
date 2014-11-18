68297249be96bc784aba773dbc06c03ba28b7891
Paul Kennedy <c10319445@mydit.ie>
2014-11-18 22:09:49 +0000

    Added Each Example

--- Playground/client/mainClient.js
+++ Playground/client/mainClient.js
@@ -3,6 +3,14 @@ if(Meteor.isClient){
 		name : "Test"
 	});
 
+	Template.nametag.helpers({
+		name: "Paul Kennedy"
+	});
+
+	Template.list.helpers({
+		people: [{name: "John"}, {name : "Paul"}, {name: "Mark"}]
+	});
+
 	Template.hello.events({
 		'click input': function(){
 			if(typeof console !== 'undefined'){
